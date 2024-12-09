49c41915f84f6ff59f367c8ebdb74f2d528af45b
Tommy <tommyburholt@icloud.com>
2024-12-09 14:41:13 +0000

    Removed newscore variable in SatisfactionMeter.java since it was redundant

--- core/src/main/java/CampusTycoon/GameLogic/Buildings/Cafeteria.java
+++ core/src/main/java/CampusTycoon/GameLogic/Buildings/Cafeteria.java
@@ -7,15 +7,18 @@ public class Cafeteria extends Building {
 	public static final String defaultImage = "Buildings\\Cafeteria.png";
 	public static final String buildingName = "Cafeteria";
 	public static int width = 3, height = 3;
-	
+
+
 	public Cafeteria(Coordinate Position) {
 		super(Position, defaultImage, width , height);
+        score = 300;
 	}
 	public Cafeteria() {
 		super(new Coordinate(0, 0), defaultImage, width , height);
+        score = 300;
 	}
-	
-	
+
+
 	@Override
 	public void incrementBuildingCounter() {
 		BuildingCounter.increaseBuildingCounter(buildingName, 1);
--- core/src/main/java/CampusTycoon/GameLogic/Event.java
+++ core/src/main/java/CampusTycoon/GameLogic/Event.java
@@ -12,23 +12,23 @@ public class Event {
 	public int choices; // Number of choices for the event
 	public String eventText; // Description of the event
 	public List<String> choiceText; // Text to be displayed in the choice buttons (NOT IMPLEMENTED)
-	
+
 	public Event() {
 		eventText = "Test event";
 		choices = 3;
 		choiceText = new ArrayList<String>(Arrays.asList(
 			"Accept", "Neutral", "Reject"));
-		
+
 		eventUI = new EventPopup(this);
 		eventUI.initialise();
 	}
-	
+
 	public void chooseOption(int option) {
 		if (option > choices) {
 			System.out.print("Invalid event choice");
 			return;
 		}
-		
+
 		switch (option) {
 			case 1:
 				Option1();
@@ -44,20 +44,20 @@ public class Event {
 				break;
 		}
 	}
-	
+
 	// Temporary choice implementations, will change to abstract functions later (as each individual event should decide what the outcome of choices are)
 	public void Option1() {
-		SatisfactionMeter.increaseSatisfactionScore(4);
+		SatisfactionMeter.modifySatisfactionScore(4);
 		this.End();
 	}
 	public void Option2() {
 		this.End();
 	}
 	public void Option3() {
-		SatisfactionMeter.decreaseSatisfactionScore(4);
+		SatisfactionMeter.modifySatisfactionScore(-4);
 		this.End();
 	}
-	
+
 	public void End() {
 		eventUI.close();
 		GameUtils.currentEvent = null;
