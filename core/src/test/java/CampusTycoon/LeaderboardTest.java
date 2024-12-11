baf49a8567d2e2091e2cdfb5f0d9248e3a6a324e
Damo <tcw524@york.ac.uk>
2024-12-11 18:42:32 +0000

    setup junit

---
+++ core/src/test/java/CampusTycoon/LeaderboardTest.java
@@ -0,0 +1,16 @@
+package CampusTycoon;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class LeaderboardRest {
+    
+    @Test
+    public void testFileRead(){
+
+        assertTrue(true);
+
+    }
+
+}
