accf48a47283bfd079368c62ade6bfc2ec879227
Damo <tcw524@york.ac.uk>
2025-01-02 19:28:54 +0000

    Adding unit tests

---
+++ core/src/test/java/CampusTycoon/UI/Components/BankruptMenuTest.java
@@ -0,0 +1,18 @@
+package CampusTycoon.UI.Components;
+
+import org.junit.jupiter.api.Test;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+
+public class BankruptMenuTest {
+    
+
+    @Test
+    void testCreateBankruptMenu(){
+
+
+
+    }
+
+    
+}
